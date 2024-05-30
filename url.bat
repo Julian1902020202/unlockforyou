4a28a90e14041ab933738d30ca52d24618e03e35
Julian1902 <julian.stoffels123@gmail.com>
2024-05-30 18:14:10 +0200

    GaYD5ow2cTEp

--- url.bat
+++ url.bat
@@ -1,5 +1,20 @@
+setlocal EnableDelayedExpansion
+
+rem Generiere eine zufällige 12-stellige Zeichenfolge
+set "charset=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+set "randomString="
+for /L %%i in (1,1,12) do (
+    set /A "randNum=!random! %% 62"
+    for %%j in (!randNum!) do set "randomChar=!charset:~%%j,1!"
+    set "randomString=!randomString!!randomChar!"
+)
+
+rem Git-Befehle mit der generierten Zeichenfolge
 git add .
-git commit -m "123456"
+git commit -m "!randomString!"
 git push origin master
 git push heroku master
+
+endlocal
+
  
